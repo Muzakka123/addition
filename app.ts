2724b5ab4bddf16292d14ba07cf447fda6829707
muzakka mir <159703338+Muzakka123@users.noreply.github.com>
2024-05-16 16:58:53 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,5 @@
+//exercise 10
+//my name is MUZAKKA
+//Dated: 6/03/2024
+// this progress will run  simple code just like hello world
+console.log(`hello world simple progress`);
---
+++ program2.js
@@ -0,0 +1,4 @@
+//exercise 10
+//my name is MUZAKKA
+//this progress will multiplication
+console.log(5 * 2);
